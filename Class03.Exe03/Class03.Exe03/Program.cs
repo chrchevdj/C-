af484a32f5ebc7bc4c211a53a788d52eac1d5cfa
chrchevdj <118832611+chrchevdj@users.noreply.github.com>
2023-03-12 22:32:14 +0200

    Add files via upload

---
+++ Class03.Exe03/Class03.Exe03/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Please enter the number:");
+
+string inputNumberOne = Console.ReadLine();
+int numberOne;
+
+bool numberOneSuccessParse = int.TryParse(inputNumberOne, out numberOne);
+
+if (!numberOneSuccessParse)
+{
+    Console.WriteLine("Wrong input");
+    return;
+}
+int counter = 0;
+for (int i = 1; i < numberOne; i++)
+{
+    if (i % 3 != 0 && i % 7 != 0)
+    {
+    counter--;
+    Console.WriteLine($"Message no. {i}");
+    }
+    if (i == 100) 
+    {
+    Console.WriteLine("The limit is reached");
+    break;
+    }
+  }
+
+Console.ReadKey();
