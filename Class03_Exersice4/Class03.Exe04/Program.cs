3e6cda861cad314676ba0bc854ca5bd01a8497da
chrchevdj <118832611+chrchevdj@users.noreply.github.com>
2023-03-27 21:42:54 +0300

    Add files via upload

---
+++ Class03_Exersice4/Class03.Exe04/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Class03.Exe04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string[] wordsarray = { "Djoko", "Chrchev" };
+            foreach (string word in wordsarray)
+            {
+                Console.WriteLine(word);
+            }
+            decimal[] decimalarray = { 1.00m, 2.00m };
+            foreach (decimal dec in decimalarray)
+            {
+                Console.WriteLine(dec);
+            }
+            Console.WriteLine(decimalarray);
+            char[] chararray = { 'A', 'B' };
+            Console.WriteLine(chararray);
+            bool[] boolarray = { true, false };
+            Console.WriteLine(boolarray);
+            int[][] intarr = { new int[] { 1, 2 }, new int[] { 3, 4 } };
+            Console.WriteLine(intarr);
+
+        }
+        
+
+    }
+}
---
+++ Class03_Exersice5/Class03.Exe05/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Class03.Exe05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int[] ints = new int[5];
+            for (int i = 0; i < ints.Length; i++) 
+            { 
+                Console.Write("Enter the number ", i);
+                ints[i] = int.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            foreach (int i in ints)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"the sum of the five numbers is {sum}");
+        }
+    }
+}
